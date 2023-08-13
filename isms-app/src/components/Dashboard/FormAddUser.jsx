338b9a184280dbb440dfcef40d89a0507bdb54a7
King Wisdom <tue.hoang@icetea.io>
2023-08-13 09:08:24 +0700

    fix: ui admin users

--- isms-app/src/components/Dashboard/FormAddUser.jsx
+++ isms-app/src/components/Dashboard/FormAddUser.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import DropdownRole from "./TableUsers/DropdownRole";
 import { roles } from "./TableRoles";
+import MessageError from "./MessageError";
 
 const FormAddUser = ({ open, setOpen }) => {
   const [role, setRole] = useState(roles[0]);
@@ -15,46 +16,95 @@ const FormAddUser = ({ open, setOpen }) => {
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-50 bg-[#D9D9D950]" />
-        <Dialog.Content className="fixed left-[50%] top-[50%] z-50 max-h-[85vh] w-[90vw] max-w-[601px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white focus:outline-none flex flex-col border-2 border-[#BFBFBF]">
+        <Dialog.Content className="fixed left-[50%] top-[50%] z-50 max-h-[85vh] w-[90vw] max-w-[664px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white focus:outline-none flex flex-col border-2 border-[#BFBFBF]">
           <div className="py-6 px-10">
-            <h6 className="text-xl xl:text-3xl font-semibold">Add new user</h6>
+            <h6 className="text-xl xl:text-3xl font-semibold uppercase text-center">
+              New user
+            </h6>
 
-            <div className="space-x-5 xl:space-x-8 mt-3">
-              <div>
-                <label className="text-[#647186] text-base font-semibold xl:text-lg">
-                  Work Email
+            <div className="mt-10 space-y-8">
+              <div className="flex items-start">
+                <label className="text-[#647186] text-base font-semibold xl:text-lg w-[150px]">
+                  Email
                 </label>
-                <input className="mt-3 w-full rounded-lg py-1.5 px-5 bg-[#F1F1F1] focus:outline-none text-black" />
+                <div className="flex-1">
+                  <input
+                    className="w-full rounded-lg py-1.5 px-5 border-2 border-[#CCC9C9] focus:outline-none text-black"
+                    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+                  />
+                  <MessageError type="small" error={"Wrong email format"} />
+                </div>
               </div>
-            </div>
-            <div className="space-x-5 xl:space-x-8 mt-3">
-              <div>
-                <label className="text-[#647186] text-base font-semibold xl:text-lg">
-                  Role
+              <div className="flex items-start">
+                <label className="text-[#647186] text-base font-semibold xl:text-lg w-[150px]">
+                  Password
                 </label>
-                <DropdownRole
-                  selected={role}
-                  setSelected={setRole}
-                  className="border-0 rounded-lg bg-[#F1F1F1] text-sm xl:text-lg py-0.5"
-                />
+                <div className="flex-1">
+                  <input
+                    type="password"
+                    className="w-full rounded-lg py-1.5 px-5 border-2 border-[#CCC9C9] focus:outline-none text-black"
+                    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+                  />
+                  <MessageError
+                    type="small"
+                    error={"Consists of 10 number and does not include letters"}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="flex space-x-5 justify-end w-full mt-6 xl:mt-10">
-              <button
-                className="text-sm xl:text-lg w-[115px] py-1.5 bg-[#043AC5] text-white focus:outline-none border-0 font-semibold"
-                onClick={handleAdd}
-              >
-                Add
-              </button>
-              <button
-                className="text-sm xl:text-lg w-[115px] py-1.5 text-black bg-[#D9D9D9] focus:outline-none border-0 font-semibold"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setOpen(false);
-                }}
-              >
-                Cancel
-              </button>
+              <div className="flex items-start">
+                <label className="text-[#647186] text-base font-semibold xl:text-lg w-[150px]">
+                  First Name
+                </label>
+                <div className="flex-1">
+                  <input
+                    className="w-full rounded-lg py-1.5 px-5 border-2 border-[#CCC9C9] focus:outline-none text-black"
+                    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+                  />
+                  {/* <MessageError type="small" error={"First Name is required"} /> */}
+                </div>
+              </div>
+              <div className="flex items-start">
+                <label className="text-[#647186] text-base font-semibold xl:text-lg w-[150px]">
+                  Middle Name
+                </label>
+                <div className="flex-1">
+                  <input
+                    className="w-full rounded-lg py-1.5 px-5 border-2 border-[#CCC9C9] focus:outline-none text-black"
+                    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+                  />
+                  {/* <MessageError type="small" error={"Middle Name is required"} /> */}
+                </div>
+              </div>
+              <div className="flex items-start">
+                <label className="text-[#647186] text-base font-semibold xl:text-lg w-[150px]">
+                  Last Name
+                </label>
+                <div className="flex-1">
+                  <input
+                    className="w-full rounded-lg py-1.5 px-5 border-2 border-[#CCC9C9] focus:outline-none text-black"
+                    style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
+                  />
+                  <MessageError type="small" error={"Last Name is required"} />
+                </div>
+              </div>
+
+              <div className="flex space-x-5 justify-center w-full mt-6 xl:mt-10">
+                <button
+                  className="text-sm xl:text-lg w-[150px] py-2 bg-[#4AA976] text-white focus:outline-none border-0 font-semibold"
+                  onClick={handleAdd}
+                >
+                  Add user
+                </button>
+                {/* <button
+                  className="text-sm xl:text-lg w-[115px] py-1.5 text-black bg-[#D9D9D9] focus:outline-none border-0 font-semibold"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                  }}
+                >
+                  Cancel
+                </button> */}
+              </div>
             </div>
           </div>
         </Dialog.Content>
